32c6b2c4358199869149573fe5eecdc272d55298
Christopher Mayes <cmayes@cori19.nersc.gov>
2019-06-15 17:22:27 -0700

    Astra driver, with kekgun example

--- xopt/configure.py
+++ xopt/configure.py
@@ -60,6 +60,20 @@ def load_config(filePath):
     'output_dir':c.get('output_dir', '.')
     }
     
+    
+
+    #---------------------------      
+    # astra_config
+    if 'astra_config' in config:
+        c = config['astra_config']
+        d['astra_config'] = {'timeout':c.getint('timeout', None),
+                           'workdir':c.get('workdir', '/tmp'),    # Workdir must be set. Otherswise, work will be done in template!
+                           'verbose':c.getint('workdir', 0),
+                           'astra_bin':c.get('astra_bin', '$ASTRA_BIN'),
+                           'generator_bin':c.get('generator_bin', '$GENERATOR_BIN')
+                             
+        }    
+    
     #---------------------------      
     # gpt_config
     if 'gpt_config' in config:
@@ -85,6 +99,7 @@ def load_config(filePath):
             'skip_checkpoint_eval':c.getboolean('skip_checkpoint_eval', False),
             'population_size':c.getint('population_size', 0), # 0 is intended to be automatiaclly adjusted. 
             'max_generations':c.getint('max_generations', 100),
+            'checkpoint':c.get('checkpoint', ''),
             'checkpoint_frequency':c.getint('checkpoint_frequency', 1),
             'verbose':c.getboolean('verbose', True)
         }
@@ -107,9 +122,21 @@ def configure(config):
     c['xopt_config']['vocs_file'] = full_path(config['xopt_config']['vocs_file'])
     c['xopt_config']['output_dir'] = full_path(config['xopt_config']['output_dir'])
     
+    # vocs
     c['vocs'] = load_vocs( config['xopt_config']['vocs_file'])    
     
-    c['vocs']['template_dir'] =  full_path(c['vocs']['template_dir'])
+    # nsga2
+    if 'nsga2_config' in c:
+        if c['nsga2_config']['checkpoint'] != '':
+            c['nsga2_config']['checkpoint'] = full_path(c['nsga2_config']['checkpoint'])
+    
+    # Options for templates
+    if 'template_dir' in c['vocs']:
+        c['vocs']['template_dir'] =  full_path(c['vocs']['template_dir'])
+    if 'templates' in c['vocs']:
+        for k in c['vocs']['templates']:
+            c['vocs']['templates'][k] = full_path(c['vocs']['templates'][k])
+    
     
     # Binary paths
     # Prepend to $PATH
@@ -117,6 +144,11 @@ def configure(config):
         c['gpt_config']['gpt_path'] = add_to_path( config['gpt_config']['gpt_path'] )
     if 'distgen_config' in c:
         c['distgen_config']['distgen_path'] =  add_to_path(config['distgen_config']['distgen_path'])
+     
+    # Executables 
+    if 'astra_config' in c:
+        c['astra_config']['astra_bin'] = full_path(config['astra_config']['astra_bin'])
+        c['astra_config']['generator_bin'] = full_path(config['astra_config']['generator_bin'])
     
     return c
     
