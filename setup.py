df79ebccda83e9da8f724faafe9578902a86d084
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2020-01-09 14:06:38 -0800

    cleaning up for conda

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from os import path, environ
+
+cur_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
+    requirements = f.read().split()
+
+
+
+setup(
+    name='xopt',
+    version = 'v0.1.0',
+    packages=find_packages(),  
+    package_dir={'xopt':'xopt'},
+    url='https://github.com/ChristopherMayes/xopt',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires='>=3.6'
+)
