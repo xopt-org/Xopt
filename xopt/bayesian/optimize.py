84b5fd043c4b21895639c94e2dd344a55e0ba7d8
Ryan Roussel <rroussel@uchicago.edu>
2021-08-09 16:05:10 -0500

    Created classes for generators -> mobo and bayes_exp

--- xopt/bayesian/optimize.py
+++ xopt/bayesian/optimize.py
@@ -1,18 +1,17 @@
 import logging
 import os
 import sys
-from concurrent.futures import Executor
 
 import torch
 from botorch.models.transforms import Standardize
 from botorch.models.transforms.input import Normalize
 from botorch.utils.sampling import draw_sobol_samples
 
-
 from .data import save_data_dict, get_data_json
 from .models.models import create_model
 from .utils import standardize, collect_results, sampler_evaluate, get_corrected_outputs, NoValidResultsError
 from ..tools import full_path, DummyExecutor
+from .generators import exploration, mobo
 
 """
     Bayesian Exploration Botorch
@@ -21,19 +20,41 @@ from ..tools import full_path, DummyExecutor
 
 tkwargs = {
     "dtype": torch.double,
-    "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+    "device": torch.device("cpu"),
 }
 
 # Logger
 logger = logging.getLogger(__name__)
 
 
-def bayesian_optimize(config, evaluate_f,
-                      gen_candidate):
+def bayesian_optimize(vocs,
+                      evaluate_f,
+                      candidate_generator,
+                      output_path=None,
+                      custom_model=None,
+                      executor=DummyExecutor(),
+                      restart_file=None,
+                      initial_x=None,
+                      n_initial_samples=1,
+                      n_steps=1,
+                      verbose=True,
+                      ref=None,
+                      ):
     """
 
     Parameters
     ----------
+    ref
+    vocs
+    verbose
+    n_steps
+    n_initial_samples
+    initial_x
+    executor
+    restart_file
+    custom_model
+    output_path
+    candidate_generator
     config : dict
         Varabiles, objectives, constraints and statics dictionary, see xopt documentation for detials
 
@@ -52,41 +73,20 @@ def bayesian_optimize(config, evaluate_f,
         Dictionary object containing optimization points + other info
 
     """
-    vocs = config['vocs']
 
-    if config['algorithm']['options']['eval_args'] is None:
-        eval_args = []
-    else:
-        eval_args = config['algorithm']['options']['eval_args']
-
-    if not config['algorithm']['options']['use_gpu']:
-        tkwargs['device'] = 'cpu'
-
-    if config['xopt']['output_path'] is None:
-        output_path = ''
-    else:
-        output_path = config['xopt']['output_path']
+    # allow string specification of generator objects with default values
+    if candidate_generator == 'mobo':
+        candidate_generator = mobo.MOBOGenerator(ref)
+    elif candidate_generator == 'bayes_exp':
+        candidate_generator = exploration.BayesianExplorationGenerator()
 
     # Verbose print helper
     def vprint(*a, **k):
         # logger.debug(' '.join(a))
         # TODO: use logging instead of print statements
-        if config['algorithm']['options']['verbose']:
+        if verbose:
             print(*a, **k)
             sys.stdout.flush()
-
-    # set up an executor
-    if not isinstance(config['algorithm']['options']['executor'], Executor):
-        executor = DummyExecutor()
-        vprint('No executor given. Running in serial mode.')
-    else:
-        executor = config['algorithm']['options']['executor'].copy()
-    config['algorithm']['options']['executor'] = str(type(executor))
-
-    # set the custom model
-    # TODO: point json results file to model location file
-    custom_model = config['algorithm']['options']['custom_model']
-    #config['algorithm']['options']['custom_model'] = str(type(custom_model))
 
     # Setup saving to file
     if output_path:
@@ -114,29 +114,29 @@ def bayesian_optimize(config, evaluate_f,
     # inputs are normalized in [0,1]
     input_normalize = Normalize(len(variable_names), bounds)
 
-    sampler_evaluate_args = {'verbose': config['algorithm']['options']['verbose']}
+    sampler_evaluate_args = {'verbose': verbose}
 
     # generate initial samples if no initial samples are given
-    if config['algorithm']['options']['restart_data_file'] is None:
-        if config['algorithm']['options']['initial_x'] is None:
-            initial_x = draw_sobol_samples(bounds, 1, config['algorithm']['options']['n_initial_samples'])[0]
+    if restart_file is None:
+        if initial_x is None:
+            initial_x = draw_sobol_samples(bounds, 1, n_initial_samples)[0]
         else:
-            initial_x = config['algorithm']['options']['initial_x']
+            initial_x = initial_x
 
         # submit evaluation of initial samples
         initial_y = [executor.submit(sampler_evaluate,
                                      dict(zip(variable_names, x.cpu().numpy())),
-                                     evaluate_f, *eval_args,
+                                     evaluate_f,
                                      **sampler_evaluate_args) for x in initial_x]
 
         train_x, train_y, train_c = collect_results(initial_y, vocs, **tkwargs)
 
     else:
-        train_x, train_y, train_c = get_data_json(config['algorithm']['options']['restart_data_file'],
+        train_x, train_y, train_c = get_data_json(restart_file,
                                                   vocs, **tkwargs)
 
     # do optimization
-    for i in range(config['algorithm']['options']['n_steps']):
+    for i in range(n_steps):
 
         # get corrected values
         corrected_train_y, corrected_train_c = get_corrected_outputs(vocs, train_y, train_c)
@@ -151,15 +151,15 @@ def bayesian_optimize(config, evaluate_f,
                              custom_model)
 
         # get candidate point(s)
-        candidates = gen_candidate(model, bounds, vocs, config['algorithm']['options'])
+        candidates = candidate_generator.generate(model, bounds, vocs)
 
-        if config['algorithm']['options']['verbose']:
+        if verbose:
             vprint(f'Candidate(s): {candidates}')
 
         # observe candidates
         fut = [executor.submit(sampler_evaluate,
                                dict(zip(variable_names, x.cpu().numpy())),
-                               evaluate_f, *eval_args,
+                               evaluate_f,
                                **sampler_evaluate_args) for x in candidates]
         try:
             new_x, new_y, new_c = collect_results(fut, vocs, **tkwargs)
@@ -175,7 +175,7 @@ def bayesian_optimize(config, evaluate_f,
             constraint_status = corrected_train_c < 0.0
 
             full_data = torch.hstack((train_x, train_y, train_c, constraint_status, feas))
-            save_data_dict(config, full_data)
+            # save_data_dict(config, full_data)
 
         except NoValidResultsError:
             print('No valid results found, skipping to next iteration')
