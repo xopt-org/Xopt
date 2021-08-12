80e8ded55ed45d6c0f87df04206a239f17facb3f
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-08-12 13:48:51 -0700

    New version importing: `from xopt import __version__`

--- xopt/__init__.py
+++ xopt/__init__.py
@@ -1,6 +1,5 @@
-from .xopt import Xopt
-from .tools import xopt_logo
-from ._version import __version__
-
 from . import _version
 __version__ = _version.get_versions()['version']
+
+from .xopt import Xopt
+from .tools import xopt_logo
