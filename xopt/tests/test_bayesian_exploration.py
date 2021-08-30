04f42cce4e71a97df92bde20771b372462cccda3
Ryan Roussel <rroussel@uchicago.edu>
2021-08-30 15:08:58 -0500

    Updated tests

--- xopt/tests/test_bayesian_exploration.py
+++ xopt/tests/test_bayesian_exploration.py
@@ -1,70 +1,42 @@
+import copy
+
 from xopt import Xopt
-
-import yaml
-
-# Make a proper input file.
-YAML = """
-xopt: {output_path: null, verbose: true}
-
-algorithm:
-  name: bayesian_exploration
-  options: {n_initial_samples: 5,
-            n_steps: 2, 
-            generator_options: {batch_size: 2},
-            verbose: True}
-
-simulation: 
-  name: test_TNK
-  evaluate: xopt.evaluators.test_TNK.evaluate_TNK  
-
-vocs:
-  name: TNK_test
-  description: null
-  simulation: test_TNK
-  templates: null
-  variables:
-    x1: [0, 3.14159]
-    x2: [0, 3.14159]
-  objectives: {y1: MINIMIZE, y2: MINIMIZE}
-  constraints:
-    c1: [GREATER_THAN, 0]
-    c2: [LESS_THAN, 0.5]
-  constants: {a: dummy_constant}
-
-"""
-
-YAML2 = """
-xopt: {output_path: null, verbose: true}
-
-algorithm:
-  name: bayesian_exploration
-  options: {n_initial_samples: 5,
-            n_steps: 2, 
-            generator_options: {batch_size: 1, sigma: [[1.0,0.0],[0.0,1.0]]},
-            verbose: True}
-
-simulation: 
-  name: test_TNK
-  evaluate: xopt.evaluators.test_TNK.evaluate_TNK  
-
-vocs:
-  name: TNK_test
-  description: null
-  simulation: test_TNK
-  templates: null
-  variables:
-    x1: [0, 3.14159]
-    x2: [0, 3.14159]
-  objectives: {y1: MINIMIZE, y2: MINIMIZE}
-  constraints:
-    c1: [GREATER_THAN, 0]
-    c2: [LESS_THAN, 0.5]
-  constants: {a: dummy_constant}
-
-"""
+import pytest
+from ..evaluators import test_TNK
 
 
-def test_bayes_exp_tnk():
-    for ele in [YAML, YAML2]:
-        X = Xopt(yaml.safe_load(ele))
+class TestClassBayesExp:
+    VOCS = test_TNK.VOCS
+    config = {'vocs': test_TNK.VOCS.copy()}
+    config['simulation'] = {'name': 'test_TNK',
+                            'evaluate': 'xopt.evaluators.test_TNK.evaluate_TNK'}
+    config['xopt'] = {'output_path': '', 'verbose': False, 'algorithm': 'mobo'}
+    config['algorithm'] = {'name': 'bayesian_exploration',
+                           'options': {
+                               'n_initial_samples': 1,
+                               'n_steps': 1,
+                               'generator_options': {
+                                   'mc_samples': 4,
+                                   'num_restarts': 1,
+                                   'raw_samples': 4
+                               }
+                           }}
+
+    def test_bayes_exp_base(self):
+        test_config = copy.deepcopy(self.config)
+        X = Xopt(test_config)
         X.run()
+
+        X = Xopt(test_config)
+        X.run()
+
+    def test_batch(self):
+        test_config = copy.deepcopy(self.config)
+        test_config['algorithm']['options']['generator_options'] = {
+            'batch_size': 2
+        }
+
+        X = Xopt(test_config)
+        X.run()
+
+
