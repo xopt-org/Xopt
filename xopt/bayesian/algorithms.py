28075da18dabdba3c6fe62f004fb0135f257f564
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-10-04 10:28:58 -0700

    indent

--- xopt/bayesian/algorithms.py
+++ xopt/bayesian/algorithms.py
@@ -16,53 +16,53 @@ def bayesian_optimize(vocs, evaluate_f,
                       initial_x=None,
                       verbose=True,
                       generator_options=None):
-     """
-     Multi-objective Bayesian optimization
+    """
+    Multi-objective Bayesian optimization
 
-     Parameters
-     ----------
-     vocs : dict
-         Varabiles, objectives, constraints and statics dictionary,
-         see xopt documentation for detials
+    Parameters
+    ----------
+    vocs : dict
+        Varabiles, objectives, constraints and statics dictionary,
+        see xopt documentation for detials
 
-     evaluate_f : callable
-         Returns dict of outputs after problem has been evaluated
+    evaluate_f : callable
+        Returns dict of outputs after problem has been evaluated
 
-     n_steps : int, default = 1
-         Number of optimization steps to execute
+    n_steps : int, default = 1
+        Number of optimization steps to execute
 
-     n_initial_samples : int, defualt = 1
-         Number of initial samples to take before using the model,
-         overwritten by initial_x
+    n_initial_samples : int, defualt = 1
+        Number of initial samples to take before using the model,
+        overwritten by initial_x
 
-     output_path : str, default = ''
-         Path location to place outputs
+    output_path : str, default = ''
+        Path location to place outputs
 
-     custom_model : callable, optional
-         Function of the form f(train_inputs, train_outputs) that
-         returns a trained custom model
+    custom_model : callable, optional
+        Function of the form f(train_inputs, train_outputs) that
+        returns a trained custom model
 
-     executor : Executor, optional
-         Executor object to run evaluate_f
+    executor : Executor, optional
+        Executor object to run evaluate_f
 
-     restart_file : str, optional
-         File location of JSON file that has previous data
+    restart_file : str, optional
+        File location of JSON file that has previous data
 
-     initial_x : list, optional
-         Nested list to provide initial candiates to evaluate, overwrites n_initial_samples
+    initial_x : list, optional
+        Nested list to provide initial candiates to evaluate, overwrites n_initial_samples
 
-     verbose : bool, defualt = True
-         Print out messages during optimization
+    verbose : bool, defualt = True
+        Print out messages during optimization
 
-     generator_options : dict
-         Dictionary of options for MOBO
+    generator_options : dict
+        Dictionary of options for MOBO
 
-     Returns
-     -------
-     results : dict
-         Dictionary with output data at the end of optimization
+    Returns
+    -------
+    results : dict
+        Dictionary with output data at the end of optimization
 
-     """
+    """
 
     try:
         # Required
