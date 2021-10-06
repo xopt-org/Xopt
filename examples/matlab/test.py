2b35e7f06031e79b72daf61a6f4c707968aff9ff
Glen <whitegr@slac.stanford.edu>
2021-10-05 17:16:35 -0700

    fixed test script using feedback from Mathworks

--- examples/matlab/test.py
+++ examples/matlab/test.py
@@ -1,18 +1,12 @@
 import matlab.engine
-names = matlab.engine.find_matlab()
-def f():
-    
-    
-    eng_id = names[0]
-    # eng = matlab.engine.connect_matlab(eng_id)
-    eng = matlab.engine.start_matlab()
+def f(eng):
     x1 = 1.
     x2 = 2.
     result = eng.testeval(x1,x2,nargout=2)
     
-    print(result)
-    
-    
+names = matlab.engine.find_matlab()
+eng_id = names[0]
+eng = matlab.engine.connect_matlab(eng_id)
 for i in range(10000):
     print(i)
-    f()
+    f(eng)
