8de3fc1631a8cc638308ec1623333433d49f0280
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2022-04-06 18:12:25 -0700

    Simplify RandomGenerator

--- xopt/generators/random.py
+++ xopt/generators/random.py
@@ -5,22 +5,10 @@ import pandas as pd
 
 from xopt.generator import Generator
 
-
 class RandomGenerator(Generator):
     def __init__(self, vocs):
         super(RandomGenerator, self).__init__(vocs)
 
     def generate(self, n_candidates) -> List[Dict]:
         """generate uniform random data points"""
-        problem_dim = len(self.vocs.variables)
-        random_vals = np.random.rand(n_candidates, problem_dim)
-
-        # scale according to vocs limits
-        varibles = self.vocs.variables
-        sorted_dict = {k: varibles[k] for k in sorted(varibles)}
-        for idx, item in enumerate(sorted_dict.items()):
-            random_vals[:, idx] = (
-                random_vals[:, idx] * (item[1][1] - item[1][0]) + item[1][0]
-            )
-
-        return self.convert_numpy_to_inputs(random_vals)
+        return [self.vocs.random_inputs() for _ in range(n_candidates)]
