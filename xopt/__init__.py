740cac5c56265829c27f0efd970680c3ab04c7a6
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2022-04-13 16:07:00 -0700

    Restore versioneer and logging

--- xopt/__init__.py
+++ xopt/__init__.py
@@ -2,3 +2,14 @@ from .evaluator import Evaluator
 from .generator import Generator
 from .vocs import VOCS
 from .base import XoptBase
+
+from . import _version
+__version__ = _version.get_versions()['version']
+
+from xopt.log import configure_logger
+
+def output_notebook():
+    """
+    Redirects logging to stdout for use in Jupyter notebooks
+    """
+    configure_logger()
