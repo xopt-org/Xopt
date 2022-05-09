1d363107413e6d75e21712ed6d094e3fcd0258c7
Ryan Roussel <rroussel@slac.stanford.edu>
2022-05-09 11:49:50 -0500

    remove logging stuff

--- xopt/options.py
+++ xopt/options.py
@@ -1,13 +1,4 @@
-from enum import Enum
-
 from pydantic import BaseModel, Field
-
-
-class LoggingEnum(str, Enum):
-    DEBUG = "DEBUG"
-    INFO = "INFO"
-    WARNING = "WARNING"
-    ERROR = "ERROR"
 
 
 class XoptOptions(BaseModel):
@@ -25,10 +16,3 @@ class XoptOptions(BaseModel):
     dump_file: str = Field(
         None, description="file to dump the results of the evaluations"
     )
-    #logging_level: LoggingEnum = Field(
-    #    LoggingEnum.INFO, description="logging level for package logging"
-    #)
-    #logging_file: str = Field(None, description="file to dump the logs of the package")
-
-    #class Config:
-    #    use_enum_values = True
