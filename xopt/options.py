82119710666b97b2f681e60025f019ff181d1db6
Ryan Roussel <rroussel@slac.stanford.edu>
2022-05-09 10:58:44 -0500

    add in template code for logging options

--- xopt/options.py
+++ xopt/options.py
@@ -1,4 +1,13 @@
+from enum import Enum
+
 from pydantic import BaseModel, Field
+
+
+class LoggingEnum(str, Enum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
 
 
 class XoptOptions(BaseModel):
@@ -8,7 +17,7 @@ class XoptOptions(BaseModel):
     strict: bool = Field(
         False,
         description="flag to indicate if exceptions raised during evaluation "
-                    "should stop Xopt",
+        "should stop Xopt",
     )
     timeout: float = Field(
         None, description="maximum waiting time during `Xopt.step()`"
@@ -16,3 +25,10 @@ class XoptOptions(BaseModel):
     dump_file: str = Field(
         None, description="file to dump the results of the evaluations"
     )
+    #logging_level: LoggingEnum = Field(
+    #    LoggingEnum.INFO, description="logging level for package logging"
+    #)
+    #logging_file: str = Field(None, description="file to dump the logs of the package")
+
+    #class Config:
+    #    use_enum_values = True
