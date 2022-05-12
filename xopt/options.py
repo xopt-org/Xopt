6a48d612ccadde51d276e2217201014d7158787b
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2022-05-12 10:18:43 -0700

    XoptOptions as XoptBaseModel

--- xopt/options.py
+++ xopt/options.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, Field
+from pydantic import Field
+from xopt.pydantic import XoptBaseModel
 
-
-class XoptOptions(BaseModel):
+class XoptOptions(XoptBaseModel):
     asynch: bool = Field(
         False, description="flag to evaluate and submit evaluations asynchronously"
     )
