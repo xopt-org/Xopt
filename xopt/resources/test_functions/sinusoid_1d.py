e86a3c9945a076c3328b972172eb4d85e6e6adf7
Ryan Roussel <rroussel@slac.stanford.edu>
2022-05-12 15:35:34 -0500

    Implement bilog transform

---
+++ xopt/resources/test_functions/sinusoid_1d.py
@@ -0,0 +1,22 @@
+from typing import Dict
+
+import numpy as np
+
+from xopt.vocs import VOCS
+
+sinusoid_vocs = VOCS(
+    **{
+        "variables": {"x1": [0, 1.75*3.14159]},
+        "objectives": {"y1": "MINIMIZE"},
+        "constraints": {"c1": ["GREATER_THAN", 0]},
+    }
+)
+
+
+# labeled version
+def evaluate_sinusoid(inputs: dict):
+    outputs = {
+        "y1": np.sin(inputs["x1"]),
+        "c1": 10*np.sin(inputs["x1"]) - 9.5 + np.sin(7.0*inputs["x1"]),
+    }
+    return outputs
