f001bd1e3f86cf36a2ea4efaf1f22ffa795a3232
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2022-07-01 20:18:01 -0700

    inplace writing

--- scripts/execute_notebooks.bash
+++ scripts/execute_notebooks.bash
@@ -1,11 +1,8 @@
-
-
-
 NOTEBOOKS=$(find . -type f -name "*.ipynb" -not -path '*/.*')
 echo $NOTEBOOKS
 
 for file in $NOTEBOOKS
 do
     echo "Executing $file"
-#    jupyter nbconvert --to notebook --execute $file
+    jupyter nbconvert --to notebook --execute $file --inplace
 done
