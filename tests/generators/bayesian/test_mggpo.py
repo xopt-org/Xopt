72ab035c745db570983e6d0abd50e7b12d927e7c
Ryan Roussel <rroussel@slac.stanford.edu>
2022-08-16 17:01:14 -0500

    adding data to mggpo generator + example

--- tests/generators/bayesian/test_mggpo.py
+++ tests/generators/bayesian/test_mggpo.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 
 import numpy as np
+import pandas as pd
 import pytest
 import yaml
 
@@ -17,12 +18,30 @@ class TestMGPO:
         vocs.objectives.update({"y2": "MINIMIZE"})
         MGGPOGenerator(vocs)
 
-    def test_script(self):
+    def test_serial(self):
         evaluator = Evaluator(function=evaluate_TNK)
 
         # test check options
         vocs = deepcopy(tnk_vocs)
         gen = MGGPOGenerator(vocs)
         X = Xopt(evaluator=evaluator, generator=gen, vocs=vocs)
+        X.submit_data(pd.DataFrame({"x1": [1.0, 0.75], "x2": [0.75, 1.0]}))
+        samples = X.generator.generate(10)
+        assert samples.to_numpy().shape == (10, 3)
+
         X.step()
-        X.generator.generate(10)
+
+    def test_bactched(self):
+        evaluator = Evaluator(function=evaluate_TNK)
+        evaluator.max_workers = 10
+
+        # test check options
+        vocs = deepcopy(tnk_vocs)
+        gen = MGGPOGenerator(vocs)
+        X = Xopt(evaluator=evaluator, generator=gen, vocs=vocs)
+        X.submit_data(pd.DataFrame({"x1": [1.0, 0.75], "x2": [0.75, 1.0]}))
+
+        for _ in [0, 1]:
+            X.step()
+        print(X.data)
+
