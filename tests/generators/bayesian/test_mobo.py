28fe562122e098724e9d9aff3003a6114ea5ca6d
Ryan Roussel <rroussel@slac.stanford.edu>
2022-09-21 08:48:01 -0500

    Update test_mobo.py

--- tests/generators/bayesian/test_mobo.py
+++ tests/generators/bayesian/test_mobo.py
@@ -3,9 +3,9 @@ from copy import deepcopy
 import numpy as np
 import pytest
 import yaml
+from xopt.base import Xopt
 
 from xopt.evaluator import Evaluator
-from xopt.base import Xopt
 from xopt.generators.bayesian.mobo import MOBOGenerator
 from xopt.resources.test_functions.tnk import evaluate_TNK, tnk_vocs
 from xopt.resources.testing import TEST_VOCS_BASE
