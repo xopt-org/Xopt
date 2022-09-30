377018feff6dbe111ce5da0831b2d52fe2315865
Zhe Zhang <zhezhang@slac.stanford.edu>
2022-09-30 16:10:01 -0700

    Use element-wise comparison rather than doing sum

--- tests/generators/test_extremum_seeking.py
+++ tests/generators/test_extremum_seeking.py
@@ -267,4 +267,4 @@ class TestExtremumSeekingGenerator:
         for i in range(ES_steps):
             X.step()
 
-        assert (cES - X.data['f'].to_numpy()).sum() == 0, 'Xopt ES does not match the vanilla one'
+        assert np.all(cES == X.data['f'].to_numpy()), 'Xopt ES does not match the vanilla one'
