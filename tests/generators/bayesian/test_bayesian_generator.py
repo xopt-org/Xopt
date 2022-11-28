336754eebecbb1292ac46dab757ad546127c4a7f
Ryan Roussel <rroussel@slac.stanford.edu>
2022-11-28 10:59:42 -0600

    fix max travel distances bug

--- tests/generators/bayesian/test_bayesian_generator.py
+++ tests/generators/bayesian/test_bayesian_generator.py
@@ -129,3 +129,34 @@ class TestBayesianGenerator(TestCase):
         gen.add_data(pd.DataFrame({"x1": [0.5], "x2": [5.0], "y1": [0.5], "c1": [0.5]}))
         bounds = gen._get_bounds()
         assert torch.allclose(bounds, torch.tensor([[0.4, 3.0], [0.6, 7.0]]).to(bounds))
+
+        # test with max_travel_distances specified and data
+        high_d_vocs = deepcopy(TEST_VOCS_BASE)
+        high_d_vocs.variables["x3"] = [0, 1]
+
+        defaults = BayesianGenerator.default_options()
+        defaults.optim.max_travel_distances = [0.1, 0.2, 0.1]
+
+        gen = BayesianGenerator(high_d_vocs, defaults)
+        gen.add_data(
+            pd.DataFrame(
+                {"x1": [0.5], "x2": [5.0], "x3": [0.5], "y1": [0.5], "c1": [0.5]}
+            )
+        )
+        bounds = gen._get_bounds()
+        assert torch.allclose(
+            bounds, torch.tensor([[0.4, 3.0, 0.4], [0.6, 7.0, 0.6]]).to(bounds)
+        )
+
+        # test with bad max_distances
+        defaults = BayesianGenerator.default_options()
+        defaults.optim.max_travel_distances = [0.1, 0.2]
+
+        gen = BayesianGenerator(high_d_vocs, defaults)
+        gen.add_data(
+            pd.DataFrame(
+                {"x1": [0.5], "x2": [5.0], "x3": [0.5], "y1": [0.5], "c1": [0.5]}
+            )
+        )
+        with pytest.raises(ValueError):
+            gen._get_bounds()
