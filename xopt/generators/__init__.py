c8dbcca7825a137e95e912ba291659fb2760d425
Ryan Roussel <rroussel@slac.stanford.edu>
2023-02-06 14:38:25 -0600

    add partial initialization of generators based on installed packages

--- xopt/generators/__init__.py
+++ xopt/generators/__init__.py
@@ -1,30 +1,60 @@
 from xopt.errors import XoptError
-from xopt.generators.bayesian.bayesian_exploration import BayesianExplorationGenerator
-from xopt.generators.bayesian.expected_improvement import ExpectedImprovementGenerator
-from xopt.generators.bayesian.mggpo import MGGPOGenerator
-from xopt.generators.bayesian.mobo import MOBOGenerator
-from xopt.generators.bayesian.upper_confidence_bound import (
-    TDUpperConfidenceBoundGenerator,
-    UpperConfidenceBoundGenerator,
-)
-from xopt.generators.es.extremumseeking import ExtremumSeekingGenerator
 
-from xopt.generators.ga import CNSGAGenerator
+registered_generators = []
+
+# generators needing numpy
 from xopt.generators.random import RandomGenerator
+
+# generators needing deap
+try:
+    from xopt.generators.ga import CNSGAGenerator
+
+    registered_generators += [CNSGAGenerator]
+
+except ModuleNotFoundError:
+    print("WARNING: `deap` not found, CNSGAGenerator is not available")
+
+# generators needing botorch
+try:
+    from xopt.generators.bayesian.bayesian_exploration import (
+        BayesianExplorationGenerator,
+    )
+    from xopt.generators.bayesian.expected_improvement import (
+        ExpectedImprovementGenerator,
+    )
+    from xopt.generators.bayesian.mobo import MOBOGenerator
+    from xopt.generators.bayesian.upper_confidence_bound import (
+        TDUpperConfidenceBoundGenerator,
+        UpperConfidenceBoundGenerator,
+    )
+
+    registered_generators += [
+        UpperConfidenceBoundGenerator,
+        MOBOGenerator,
+        BayesianExplorationGenerator,
+        TDUpperConfidenceBoundGenerator,
+        ExpectedImprovementGenerator,
+    ]
+
+except ModuleNotFoundError:
+    print("WARNING: `botorch` not found, Bayesian generators are not available")
+
+# generators requiring deap AND botorch
+try:
+    from xopt.generators.bayesian.mggpo import MGGPOGenerator
+
+    registered_generators += [MGGPOGenerator]
+except ModuleNotFoundError:
+    print("WARNING: `deap` and `botorch` not found, MGGPOGenerator is not available")
+
+from xopt.generators.es.extremumseeking import ExtremumSeekingGenerator
 from xopt.generators.scipy.neldermead import NelderMeadGenerator
 
 # add generators here to be registered
-registered_generators = [
-    UpperConfidenceBoundGenerator,
-    MOBOGenerator,
-    BayesianExplorationGenerator,
-    CNSGAGenerator,
+registered_generators += [
     RandomGenerator,
     NelderMeadGenerator,
-    TDUpperConfidenceBoundGenerator,
-    ExpectedImprovementGenerator,
     ExtremumSeekingGenerator,
-    MGGPOGenerator,
 ]
 
 generators = {gen.alias: gen for gen in registered_generators}
