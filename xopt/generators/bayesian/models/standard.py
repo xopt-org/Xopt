99f66192d53d33c23dff0fb9ee6e1018ffee4802
Ryan Roussel <rroussel@slac.stanford.edu>
2023-03-01 14:55:37 -0600

    implement model constructor class

--- xopt/generators/bayesian/models/standard.py
+++ xopt/generators/bayesian/models/standard.py
@@ -1,11 +1,79 @@
+import pandas as pd
 import torch
 from botorch import fit_gpytorch_mll
 from botorch.models import ModelListGP, SingleTaskGP
+from botorch.models.model import Model
 from botorch.models.transforms import Bilog, Normalize, Standardize
 from gpytorch import ExactMarginalLogLikelihood
 from gpytorch.kernels import MaternKernel, ScaleKernel
 from gpytorch.likelihoods import GaussianLikelihood
 from gpytorch.priors import GammaPrior
+from torch import Tensor
+
+from xopt.generators.bayesian.models.base import ModelConstructor
+from xopt.vocs import VOCS
+
+
+class StandardModel(ModelConstructor):
+    def __init__(self, vocs: VOCS, tkwargs, use_low_noise_prior=False):
+        super().__init__(vocs, tkwargs)
+        self.use_low_noise_prior = use_low_noise_prior
+
+        # get input transform
+        self.input_transform = Normalize(
+            self.vocs.n_variables, bounds=torch.tensor(self.vocs.bounds, **self.tkwargs)
+        )
+
+        # get likelihood
+        self.likelihood = None
+        if self.use_low_noise_prior:
+            self.likelihood = GaussianLikelihood(noise_prior=GammaPrior(1.0, 10.0))
+
+    def build_model(self, data: pd.DataFrame) -> ModelListGP:
+        """construct independent model for each objective and constraint"""
+
+        # get data
+        input_data, objective_data, constraint_data = self.vocs.extract_data(data)
+        train_X = torch.tensor(input_data.to_numpy(), **self.tkwargs)
+
+        models = []
+        for name in objective_data.keys():
+            train_Y = torch.tensor(
+                objective_data[name].to_numpy(), **self.tkwargs
+            ).unsqueeze(-1)
+            models.append(self.build_objective_model(train_X, train_Y))
+
+        for name in constraint_data.keys():
+            train_C = torch.tensor(
+                constraint_data[name].to_numpy(), **self.tkwargs
+            ).unsqueeze(-1)
+            models.append(self.build_objective_model(train_X, train_C))
+
+        return ModelListGP(*models)
+
+    def build_objective_model(self, train_X: Tensor, train_Y: Tensor) -> Model:
+        """construct model for each objective"""
+        outcome_transform = Standardize(1)
+
+        return self._build_single_task_gp(
+            train_X,
+            train_Y,
+            input_transform=self.input_transform,
+            outcome_transform=outcome_transform,
+            likelihood=self.likelihood,
+        )
+
+    def build_constraint_model(self, train_X: Tensor, train_Y: Tensor) -> Model:
+        """construct model for each constraint"""
+        outcome_transform = Bilog()
+
+        return self._build_single_task_gp(
+            train_X,
+            train_Y,
+            input_transform=self.input_transform,
+            outcome_transform=outcome_transform,
+            likelihood=self.likelihood,
+        )
 
 
 def create_standard_model(
