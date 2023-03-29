2910be111f6ef963e47c80159fec8e11c4b89624
Ryan Roussel <rroussel@slac.stanford.edu>
2023-03-29 17:12:06 -0500

    Formatting

--- xopt/resources/test_functions/ackley_20.py
+++ xopt/resources/test_functions/ackley_20.py
@@ -1,5 +1,6 @@
 import torch
 from botorch.test_functions import Ackley
+
 from xopt.vocs import VOCS
 
 variables = {f"x{i}": [-5, 10] for i in range(20)}
@@ -12,4 +13,3 @@ fun = Ackley(dim=20, negate=False)
 def evaluate_ackley(inputs: dict):
     x = torch.tensor([inputs[f"x{i}"] for i in range(20)])
     return {"f": fun(x).numpy()}
-
