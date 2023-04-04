3fac2fb3d0068b91756f12ace1a01b21bbecab6e
Ryan Roussel <rroussel@slac.stanford.edu>
2023-04-04 10:15:37 -0500

    modularize bayesian generator generate function

--- xopt/generators/bayesian/utils.py
+++ xopt/generators/bayesian/utils.py
@@ -11,3 +11,27 @@ def feasibility(X, model, sampler, vocs, posterior_transform=None):
     samples = sampler(posterior)
     objective = FeasibilityObjective(constraints)
     return torch.mean(objective(samples, X), dim=0)
+
+
+def rectilinear_domain_union(A, B):
+    """
+    outputs a rectilinear domain that is the union of bounds A/B
+
+    A.shape = (2,N)
+    B.shape = (2,N)
+    """
+    assert A.shape[0] == 2
+    assert A.shape == B.shape
+
+    out_bounds = torch.clone(A)
+
+    out_bounds[0, :] = torch.max(
+        A[0, :],
+        B[0, :],
+    )
+    out_bounds[1, :] = torch.min(
+        A[1, :],
+        B[1, :],
+    )
+
+    return out_bounds
