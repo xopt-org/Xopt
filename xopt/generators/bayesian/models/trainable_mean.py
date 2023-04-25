70fa2ba2f193fc887c95184612c0771b618e2799
Tobias Boltz <tboltz@slac.stanford.edu>
2023-04-25 11:50:46 -0700

    Simplify ModelOptions

--- xopt/generators/bayesian/models/trainable_mean.py
+++ xopt/generators/bayesian/models/trainable_mean.py
@@ -1,26 +1,14 @@
-from pydantic import Field
-
 from xopt.generators.bayesian.models.standard import StandardModelConstructor
 from xopt.generators.bayesian.models.prior_mean import CustomMean
 from xopt.generators.bayesian.options import ModelOptions
 from xopt.vocs import VOCS
 
 
-class TrainableMeanModelOptions(ModelOptions):
-    name: str = "trainable_mean_standard"
-    added_time: float = Field(
-        1.0,
-        description="additional time added to target time for optimization, "
-                    "make sure its larger than computation time for the "
-                    "GP model",
-    )
-
-
 class TrainableMeanModelConstructor(StandardModelConstructor):
-    def __init__(self, vocs: VOCS, options: TrainableMeanModelOptions):
-        if not type(options) is TrainableMeanModelOptions:
+    def __init__(self, vocs: VOCS, options: ModelOptions):
+        if not type(options) is ModelOptions:
             raise ValueError(
-                "options must be a TrainableMeanModelOptions object"
+                "options must be a ModelOptions object"
             )
         super().__init__(vocs, options)
 
