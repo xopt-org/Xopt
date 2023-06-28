60aba7147adc65321a4b3af322b92572df0b7c45
Tobias Boltz <tboltz@slac.stanford.edu>
2023-06-28 15:18:23 -0700

    Update TimeDependentModelConstructor

--- xopt/generators/bayesian/models/time_dependent.py
+++ xopt/generators/bayesian/models/time_dependent.py
@@ -1,5 +1,7 @@
+from typing import Union
 from copy import deepcopy
 
+import torch
 import pandas as pd
 
 from xopt.generators.bayesian.models.standard import StandardModelConstructor
@@ -9,12 +11,18 @@ from xopt.vocs import VOCS
 class TimeDependentModelConstructor(StandardModelConstructor):
     name = "time_dependent"
 
-    def build_model_from_vocs(self, vocs: VOCS, data: pd.DataFrame):
+    def build_model_from_vocs(
+            self,
+            vocs: VOCS,
+            data: pd.DataFrame,
+            dtype: torch.dtype = torch.double,
+            device: Union[torch.device, str] = "cpu",
+    ):
         # get max/min times
         min_t = data["time"].min()
         max_t = data["time"].max() + 15.0
         variable_dict = deepcopy(vocs.variables)
         variable_dict["time"] = [min_t, max_t]
         return self.build_model(
-            vocs.variable_names + ["time"], vocs.output_names, data, variable_dict
+            vocs.variable_names + ["time"], vocs.output_names, data, variable_dict, dtype, device
         )
