c7a5988b9c415a03660fa52b8dd206864e460d0d
Ryan Roussel <rroussel@slac.stanford.edu>
2023-07-11 15:25:24 -0500

    update standard model constructor to accept lists of points into the gp model

--- xopt/generators/bayesian/utils.py
+++ xopt/generators/bayesian/utils.py
@@ -1,6 +1,42 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
 import torch
 
 from xopt.vocs import VOCS
+
+
+def get_training_data(
+    input_names: List[str], outcome_name: str, data: pd.DataFrame
+) -> (torch.Tensor, torch.Tensor):
+    """Returns (train_X, train_Y) for the output specified by name."""
+    input_data = data[input_names]
+    outcome_data = data[outcome_name]
+
+    # handle the case where multiple data points has been passed to the input dataframe
+    expanded_input_data = []
+    expanded_outcome_data = []
+    for i, ele in enumerate(outcome_data):
+        if isinstance(ele, list) or isinstance(ele, np.ndarray):
+            expanded_outcome_data += ele
+            expanded_input_data += [input_data.iloc[i]] * len(ele)
+        else:
+            expanded_outcome_data += [ele]
+            expanded_input_data += [input_data.iloc[i]]
+
+    input_data = pd.concat(expanded_input_data, ignore_index=True, axis=1).T
+    outcome_data = pd.DataFrame(expanded_outcome_data)
+
+    # cannot use any rows where any variable values are nans
+    non_nans = ~input_data.isnull().T.any()
+    input_data = input_data[non_nans]
+    outcome_data = outcome_data[non_nans]
+
+    non_nans_output = list(~outcome_data.isnull().T.iloc[0])
+    train_X = torch.tensor(input_data[non_nans_output].to_numpy())
+    train_Y = torch.tensor(outcome_data[non_nans_output].to_numpy())
+    return train_X, train_Y
 
 
 def set_botorch_weights(weights, vocs: VOCS):
