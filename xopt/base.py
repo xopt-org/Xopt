f9f083e7e3ee0141f42050b0e503d07dd7275e4a
nikitakuklev <spontaneouscoder@gmail.com>
2023-09-13 14:11:06 -0500

    More pydantic fixes

--- xopt/base.py
+++ xopt/base.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import yaml
 from pandas import DataFrame
-from pydantic import Field, field_validator
+from pydantic import Field, field_validator, FieldValidationInfo, SerializeAsAny
 
 from xopt import _version
 from xopt.evaluator import Evaluator, validate_outputs
@@ -28,8 +28,8 @@ class Xopt(XoptBaseModel):
     """
 
     vocs: VOCS = Field(description="VOCS object for Xopt")
-    generator: Generator = Field(description="generator object for Xopt")
-    evaluator: Evaluator = Field(description="evaluator object for Xopt")
+    generator: SerializeAsAny[Generator] = Field(description="generator object for Xopt")
+    evaluator: SerializeAsAny[Evaluator] = Field(description="evaluator object for Xopt")
     strict: bool = Field(
         True,
         description="flag to indicate if exceptions raised during evaluation "
@@ -41,7 +41,7 @@ class Xopt(XoptBaseModel):
     max_evaluations: Optional[int] = Field(
         None, description="maximum number of evaluations to perform"
     )
-    data: DataFrame = Field(None, description="internal DataFrame object")
+    data: Optional[DataFrame] = Field(None, description="internal DataFrame object")
     serialize_torch: bool = Field(
         False,
         description="flag to indicate that torch models should be serialized "
@@ -61,15 +61,15 @@ class Xopt(XoptBaseModel):
 
         return value
 
-    @validator("generator", mode='before')
-    def validate_generator(cls, value, values):
+    @field_validator("generator", mode='before')
+    def validate_generator(cls, value, info: FieldValidationInfo):
         if isinstance(value, dict):
             name = value.pop("name")
             generator_class = get_generator(name)
-            value = generator_class.parse_obj({**value, "vocs": values["vocs"]})
+            value = generator_class.parse_obj({**value, "vocs": info.data["vocs"]})
         elif isinstance(value, str):
             generator_class = get_generator(value)
-            value = generator_class.parse_obj({"vocs": values["vocs"]})
+            value = generator_class.parse_obj({"vocs": info.data["vocs"]})
 
         return value
 
@@ -82,7 +82,7 @@ class Xopt(XoptBaseModel):
                 v = pd.DataFrame(v, index=[0])
         return v
 
-    @field_property
+    @property
     def n_data(self):
         if self.data is None:
             return 0
@@ -198,10 +198,10 @@ class Xopt(XoptBaseModel):
         result = self.evaluate_data(random_inputs)
         return result
 
-    def dump_state(self):
+    def dump_state(self, **kwargs):
         """dump data to file"""
         if self.dump_file is not None:
-            output = json.loads(self.json(serialize_torch=self.serialize_torch))
+            output = json.loads(self.json(serialize_torch=self.serialize_torch, **kwargs))
             with open(self.dump_file, "w") as f:
                 yaml.dump(output, f)
             logger.debug(f"Dumped state to YAML file: {self.dump_file}")
