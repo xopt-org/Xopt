7a83fe8cea01e1deb0ff7e63911100b45fe5b9cf
nikitakuklev <spontaneouscoder@gmail.com>
2023-09-15 12:08:57 -0500

    Bump python version

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=requirements,
     include_package_data=True,
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
