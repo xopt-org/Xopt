223268ed922d2cf10f05e0cfda3dbdfd176b5631
Ryan Roussel <rroussel@slac.stanford.edu>
2023-09-30 14:50:13 -0500

    fix validation issues in xopt base class and cnsga generator

--- tests/generators/ga/test_cnsga.py
+++ tests/generators/ga/test_cnsga.py
@@ -16,10 +16,34 @@ def test_cnsga():
 
 
 def test_cnsga_from_yaml():
-    X = Xopt.from_yaml(TEST_YAML)
+    YAML = """
+    max_evaluations: 5
+    dump_file: null
+    data: null
+    generator:
+        name: cnsga
+        population_size: 64
+        population_file: null  # Bad
+      
+    evaluator:
+        function: xopt.resources.test_functions.tnk.evaluate_TNK
+        function_kwargs:
+            sleep: 0
+            random_sleep: 0.1
+      
+    vocs:
+        variables:
+            x1: [0, 3.14159]
+            x2: [0, 3.14159]
+        objectives: {y1: MINIMIZE, y2: MINIMIZE}
+        constraints:
+            c1: [GREATER_THAN, 0]
+            c2: [LESS_THAN, 0.5]
+        constants: {a: dummy_constant}
+    """
+
+    X = Xopt(YAML)
     # Patch in generator
-    X.generator = CNSGAGenerator(vocs=X.vocs)
-    X.max_evaluations = 5
     X.run()
     assert len(X.data) == 5
     assert all(~X.data["xopt_error"])
