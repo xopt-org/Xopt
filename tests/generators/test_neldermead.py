67b0bddd5a58dade326fe20bd5c4c0cad601fccc
nikitakuklev <spontaneouscoder@gmail.com>
2023-10-25 15:11:34 -0500

    Add ackley test

--- tests/generators/test_neldermead.py
+++ tests/generators/test_neldermead.py
@@ -7,9 +7,21 @@ from scipy.optimize import fmin
 
 from xopt import Xopt
 from xopt.generators.scipy.neldermead import NelderMeadGenerator
-from xopt.resources.test_functions.rosenbrock import rosenbrock
+from xopt.resources.test_functions.ackley_20 import ackley, vocs as ackleyvocs
+from xopt.resources.test_functions.rosenbrock import rosenbrock, rosenbrock2_vocs as rbvocs
 from xopt.resources.testing import TEST_VOCS_BASE
 
+def compare(X, X2):
+    y = yaml.safe_load(X.yaml())
+    y2 = yaml.safe_load(X2.yaml())
+    y.pop('data')
+    y2.pop('data')
+    assert y == y2
+    # For unclear reasons, column order changes on reload....
+    data = X.data.drop(['xopt_runtime', 'xopt_error'], axis=1)
+    data2 = X2.data.drop(['xopt_runtime', 'xopt_error'], axis=1)
+    # On reload, index is not a range index anymore!
+    pd.testing.assert_frame_equal(data, data2, check_index_type=False)
 
 class TestNelderMeadGenerator:
     def test_simplex_generate_multiple_points(self):
@@ -73,35 +85,38 @@ class TestNelderMeadGenerator:
                 xbest["x0"] == result[0] and xbest["x1"] == result[1]
         ), "Xopt Simplex does not match the vanilla one"
 
-    def test_fresh_start(self):
+    @pytest.mark.parametrize("fun,fstring,x0,v",
+                             [(rosenbrock, "rosenbrock.evaluate_rosenbrock", [-1, -1], rbvocs),
+                              (ackley, "ackley_20.evaluate_ackley_np", [4]*20, ackleyvocs)])
+    def test_simplex_agreement_steps(self, fun, fstring, x0, v):
         inputs = []
 
         def wrap(x):
             inputs.append(x)
-            return rosenbrock(x)
+            return fun(x)
 
-        result = fmin(wrap, [-1, -1])
+        result = fmin(wrap, x0)
         scipy_data = np.array(inputs)
 
-        YAML = """
+        x0dict = ",".join([f"x{i}: {x0[i]} " for i in range(len(x0))])
+        YAML = f"""
                 generator:
                     name: neldermead
-                    initial_point: {x0: -1, x1: -1}
+                    initial_point: {{{x0dict}}}
                     adaptive: true
                     xatol: 0.0001
                     fatol: 0.0001
                 evaluator:
-                    function: xopt.resources.test_functions.rosenbrock.evaluate_rosenbrock
-                vocs:
-                    variables:
-                        x0: [-5, 5]
-                        x1: [-5, 5]
-                    objectives: {y: MINIMIZE}
+                    function: xopt.resources.test_functions.{fstring}
                 """
-        X = Xopt.from_yaml(YAML)
+        ym = yaml.safe_load(YAML)
+        ym['vocs'] = v.model_dump()
+        X = Xopt.from_dict(ym)
         for i in range(scipy_data.shape[0]):
             X.step()
-            print('====================')
+            data = X.vocs.variable_data(X.data).to_numpy()
+            if not np.array_equal(data, scipy_data[:i+1,:]):
+                raise Exception
 
         data = X.vocs.variable_data(X.data).to_numpy()
         assert np.array_equal(data, scipy_data)
@@ -111,45 +126,34 @@ class TestNelderMeadGenerator:
                 xbest["x0"] == result[0] and xbest["x1"] == result[1]
         ), "Xopt Simplex does not match the vanilla one"
 
-    def test_resume_consistency(self):
+    @pytest.mark.parametrize("fun,fstring,x0,v",
+                             [(rosenbrock, "rosenbrock.evaluate_rosenbrock", [-1, -1], rbvocs),
+                              (ackley, "ackley_20.evaluate_ackley_np", [4]*20, ackleyvocs)])
+    def test_resume_consistency(self, fun, fstring, x0, v):
         inputs = []
 
         def wrap(x):
             inputs.append(x)
-            return rosenbrock(x)
+            return fun(x)
 
-        result = fmin(wrap, [-1, -1])
+        result = fmin(wrap, x0)
         scipy_data = np.array(inputs)
 
-        YAML = """
+        x0dict = ",".join([f"x{i}: {x0[i]} " for i in range(len(x0))])
+        YAML = f"""
                 generator:
                     name: neldermead
-                    initial_point: {x0: -1, x1: -1}
+                    initial_point: {{{x0dict}}}
                     adaptive: true
                     xatol: 0.0001
                     fatol: 0.0001
                 evaluator:
-                    function: xopt.resources.test_functions.rosenbrock.evaluate_rosenbrock
-                vocs:
-                    variables:
-                        x0: [-5, 5]
-                        x1: [-5, 5]
-                    objectives: {y: MINIMIZE}
+                    function: xopt.resources.test_functions.{fstring}
                 """
-        X = Xopt.from_yaml(YAML)
+        ym = yaml.safe_load(YAML)
+        ym['vocs'] = v.model_dump()
+        X = Xopt.from_dict(ym)
         X.step()
-
-        def compare(X, X2):
-            y = yaml.safe_load(X.yaml())
-            y2 = yaml.safe_load(X2.yaml())
-            y.pop('data')
-            y2.pop('data')
-            assert y == y2
-            # For unclear reasons, column order changes on reload....
-            data = X.data.drop(['xopt_runtime', 'xopt_error'], axis=1)
-            data2 = X2.data.drop(['xopt_runtime', 'xopt_error'], axis=1)
-            # On reload, index is not a range index anymore!
-            pd.testing.assert_frame_equal(data, data2, check_index_type=False)
 
         for i in range(scipy_data.shape[0]-1):
             state = X.yaml()
@@ -159,19 +163,19 @@ class TestNelderMeadGenerator:
             samples = X.generator.generate(1)
             samples2 = X2.generator.generate(1)
             compare(X, X2)
-            print('>>>>>>>>>>>>>>>')
+            #print('>>>>>>>>>>>>>>>')
 
             state = X.yaml()
             X3 = Xopt.from_yaml(state)
             compare(X, X3)
-            print('>>>>>>>>>>>>>>>')
+            #print('>>>>>>>>>>>>>>>')
 
             X.evaluate_data(samples)
             X2.evaluate_data(samples2)
             X3.evaluate_data(samples2)
             compare(X, X2)
             compare(X, X3)
-            print('====================')
+            #print('====================')
 
         data = X.vocs.variable_data(X.data).to_numpy()
         assert data.shape == scipy_data.shape
@@ -180,6 +184,10 @@ class TestNelderMeadGenerator:
         data = X2.vocs.variable_data(X2.data).to_numpy()
         assert data.shape == scipy_data.shape
         # Numerical precision issues with using strings for floats
+        assert np.allclose(data, scipy_data, rtol=0, atol=1e-10)
+
+        data = X3.vocs.variable_data(X3.data).to_numpy()
+        assert data.shape == scipy_data.shape
         assert np.allclose(data, scipy_data, rtol=0, atol=1e-10)
 
         # Results should be the same
