c197c87e7d9d2f82f7ffa33b734032133a04f33c
nikitakuklev <spontaneouscoder@gmail.com>
2023-12-20 20:08:29 -0600

    Rework benchmark to use argparser

--- scripts/run_benchmarks.py
+++ scripts/run_benchmarks.py
@@ -1,3 +1,5 @@
+import argparse
+import pathlib
 import sys
 
 import pandas as pd
@@ -6,26 +8,46 @@ import pytest
 import torch
 
 from xopt.resources.benchmarking import BenchMOBO
-USE_PYTEST = True
+
 if __name__ == "__main__":
-    if USE_PYTEST:
-        # Show output results from every test function
-        # Show the message output for skipped and expected failures
-        args = ["-v", "-vrxs", "--benchmark-json=scripts/bench_output.json", "tests/benchmarks"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--direct", action="store_true", help="Run benchmarks without pytest"
+    )
+    parser.add_argument(
+        "--steps", default=20, type=int, help="Number of optimization steps"
+    )
+    parser.add_argument(
+        "--repeats", default=3, type=int, help="Number of reruns of each config"
+    )
+    parser.add_argument("--dump", default=None, type=str, help="Dump results to file")
+    args = parser.parse_args()
+
+    script_folder = pathlib.Path(__file__).parent
+
+    if not args.direct:
+        # This mode is for running from pytest, i.e. github actions, etc.
+        # Pytest will automatically determine the number of times to run
+        args = [
+            "-vrxs",
+            f"--benchmark-json={script_folder/'bench_output.json'}",
+            f"{script_folder.parent/'tests/benchmarks'}",
+        ]
 
         # Add extra arguments
         if len(sys.argv) > 1:
             args.extend(sys.argv[1:])
 
-        print("pytest benchmark arguments: {}".format(args))
         print(f"Running benchmarks on Python {sys.version}")
+        print(f"pytest benchmark arguments: {args}")
         sys.exit(pytest.main(args))
 
     else:
+        # This mode is for running/debugging directly, i.e. on a local machine
         torch.set_num_threads(1)
         df_bench_mobo = BenchMOBO.crate_parameter_table()
         bmobo = BenchMOBO(df_bench_mobo)
-        bmobo.N_STEPS = 2
+        bmobo.N_STEPS = args.steps
         N_REPEATS = 2
         results = []
         for row in df_bench_mobo.index.to_list():
@@ -36,13 +58,18 @@ if __name__ == "__main__":
         # df = pd.concat([df_bench_mobo, pd.json_normalize(df_bench_mobo['opts'])], axis=1)
         df = pd.DataFrame(results)
         # df.drop(columns=['rp'], inplace=True)
-        dfgroup = df.groupby(['k'])
+        dfgroup = df.groupby(["k"])
 
         dfsummary = dfgroup.mean(numeric_only=True)
-        dfsummary['t_std'] = dfgroup['t'].std()
-        dfsummary['hvf_std'] = dfgroup['hvf'].std()
-        dfsummary['hvf_min'] = dfgroup['hvf'].min()
-        dfsummary['hvf_max'] = dfgroup['hvf'].max()
+        tidx = dfsummary.columns.get_loc("t")
+        dfsummary.insert(loc=tidx + 1, column="t_std", value=dfgroup["t"].std())
+        dfsummary["hvf_std"] = dfgroup["hvf"].std()
+        dfsummary["hvf_min"] = dfgroup["hvf"].min()
+        dfsummary["hvf_max"] = dfgroup["hvf"].max()
 
-        pd.set_option('display.max_columns', None)
-        print(dfsummary)
+        # TODO: add tabulate for pretty-printing
+        pd.set_option("display.max_columns", None)
+        print(dfsummary.to_string(max_colwidth=20, float_format="{:.2f}".format))
+
+        if args.dump is not None:
+            dfsummary.to_csv(f"{script_folder/args.dump}")
