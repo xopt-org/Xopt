eee5c1be3993d53ab7af25c512a633b58a065186
Ryan Roussel <rroussel@slac.stanford.edu>
2024-01-30 11:22:17 -0600

    add hessian kernel and ability to remove input transforms

---
+++ xopt/generators/bayesian/custom_botorch/hessian_kernel.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import torch
+from gpytorch.kernels import RBFKernel, ScaleKernel
+from matplotlib import pyplot as plt
+from torch import Tensor
+
+from xopt.generators.bayesian.models.standard import StandardModelConstructor
+
+
+class HessianRBF(RBFKernel):
+    def __init__(self, precision_matrix: Tensor, **kwargs):
+        super().__init__(**kwargs)
+        self.lower_triangular_decomp = torch.linalg.cholesky(precision_matrix)
+
+    def forward(self, x1, x2, diag=False, **params):
+        x1_star = x1 @ self.lower_triangular_decomp
+        x2_star = x2 @ self.lower_triangular_decomp
+        return super().forward(x1_star, x2_star, diag, **params)
+
+
+if __name__ == "__main__":
+    # train_X = torch.tensor([[0.0, 0.0], [-0.8, 1]]).reshape(2, 2)
+    # train_Y = torch.tensor((1.0, 1.0)).reshape(2, 1)
+
+    cov = torch.tensor([[1, -0.8], [-0.8, 1]]).double()
+    precision = torch.linalg.inv(cov)
+    kernel = ScaleKernel(HessianRBF(precision))
+
+    data = pd.DataFrame({"x1": [0.0, 1.0], "x2": [0.0, 1.0], "y": [1.0, 0.0]})
+
+    # test in model constructor
+    constructor = StandardModelConstructor(covar_modules={"y": kernel})
+    model = constructor.build_model(
+        input_names=["x1", "x2"], outcome_names=["y"], data=data
+    )
+
+    # model = SingleTaskGP(train_X, train_Y, covar_module=kernel)
+
+    # test HessianRBF kernel
+    test_x = torch.linspace(-5.0, 5.0, 50)
+    xx = torch.meshgrid(test_x, test_x)
+    pts = torch.vstack([ele.flatten() for ele in xx]).T
+
+    with torch.no_grad():
+        post = model.posterior(pts)
+        mean = post.mean
+
+    fig, ax = plt.subplots()
+    c = ax.pcolor(*xx, mean.reshape(50, 50))
+    fig.colorbar(c)
+    plt.show()
--- xopt/generators/bayesian/utils.py
+++ xopt/generators/bayesian/utils.py
@@ -77,49 +77,6 @@ def set_botorch_weights(weights, vocs: VOCS):
     return weights
 
 
-def get_input_transform(input_names: List, input_bounds: Dict[str, List] = None):
-    """
-    Create a Botorch normalization transform for input data.
-
-    Parameters
-    ----------
-    input_names : List[str]
-        List of input feature names.
-
-    input_bounds : Optional[Dict[str, List[float]]], optional
-        A dictionary specifying the bounds for each input feature. If None,
-        no normalization is applied. The dictionary should have input feature
-        names as keys, and corresponding bounds as lists [min, max].
-
-    Returns
-    -------
-    Normalize
-        A normalization transform module.
-
-    Notes
-    -----
-    The normalization transform is applied independently to each input feature.
-
-    If `input_bounds` is provided, the transform scales each input feature to the
-    range [0, 1] based on the specified bounds. If `input_bounds` is None,
-    no normalization is applied, and the raw input values are used.
-
-    Examples
-    --------
-    >>> input_names = ['feature1', 'feature2']
-    >>> input_bounds = {'feature1': [0.0, 1.0], 'feature2': [-1.0, 1.0]}
-    >>> transform = get_input_transform(input_names, input_bounds)
-    >>> normalized_data = transform(raw_input_data)
-    """
-    if input_bounds is None:
-        bounds = None
-    else:
-        bounds = torch.vstack(
-            [torch.tensor(input_bounds[name]) for name in input_names]
-        ).T
-    return Normalize(len(input_names), bounds=bounds)
-
-
 def rectilinear_domain_union(A: torch.Tensor, B: torch.Tensor) -> torch.Tensor:
     """
     Calculate the union of two rectilinear domains represented by input bounds A and B.
