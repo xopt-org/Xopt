8087a73051340aa79cc0ef26f85b887cfefdf296
Ryan Roussel <rroussel@slac.stanford.edu>
2024-11-13 15:37:08 -0600

    add hyperparameter caching to standard model constructor

--- xopt/tests/generators/bayesian/test_model_constructor.py
+++ xopt/tests/generators/bayesian/test_model_constructor.py
@@ -521,6 +521,48 @@ class TestModelConstructor:
         assert model.models[1].likelihood.noise_covar.noise_prior.rate == 1000.0
         assert model.models[1].likelihood.noise_covar.noise_prior.concentration == 1.0
 
+    def test_model_caching(self):
+        test_data = deepcopy(TEST_VOCS_DATA)
+        test_vocs = deepcopy(TEST_VOCS_BASE)
+
+        constructor = StandardModelConstructor()
+
+        constructor.build_model(
+            test_vocs.variable_names, test_vocs.output_names, test_data
+        )
+
+        # cache model
+        old_model = constructor.build_model_from_vocs(test_vocs, test_data)
+
+        state = deepcopy(constructor._hyperparameter_store)
+        assert torch.equal(
+            old_model.models[0].covar_module.base_kernel.raw_lengthscale,
+            state["models.0.covar_module.base_kernel.raw_lengthscale"],
+        )
+
+        # add data and use the cached model hyperparameters
+        constructor.use_cached_hyperparameters = True
+        test_data = pd.concat(
+            (
+                test_data,
+                pd.DataFrame(
+                    {
+                        "x1": [0.2, 0.1],
+                        "x2": [0.2, 0.1],
+                        "y1": [0.2, 0.1],
+                        "y2": [0.2, 0.1],
+                    }
+                ),
+            )
+        )
+
+        new_model = constructor.build_model_from_vocs(test_vocs, test_data)
+        for i in range(2):
+            assert torch.equal(
+                old_model.models[i].covar_module.base_kernel.lengthscale,
+                new_model.models[i].covar_module.base_kernel.lengthscale,
+            )
+
     @pytest.fixture(autouse=True)
     def clean_up(self):
         yield
