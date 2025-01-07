ba7459960407f8eb6c7a27249af374ecc287e28d
Ryan Roussel <rroussel@slac.stanford.edu>
2025-01-07 10:00:20 -0600

    correct import

--- xopt/generators/bayesian/mggpo.py
+++ xopt/generators/bayesian/mggpo.py
@@ -2,7 +2,8 @@ from typing import Optional
 
 import pandas as pd
 import torch
-from botorch.acquisition.multi_objective import qLogNoisyExpectedHypervolumeImprovement
+from botorch.acquisition.multi_objective.logei import \
+    qLogNoisyExpectedHypervolumeImprovement
 from pydantic import Field
 
 from xopt.generators.bayesian.objectives import create_mobo_objective
