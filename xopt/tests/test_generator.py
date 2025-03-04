c3e4b97be27983e233a6eca622a5b1818f74630f
Christopher M. Pierce <chris@chris-pierce.com>
2025-03-03 21:51:45 -0800

    redo test changes

--- xopt/tests/test_generator.py
+++ xopt/tests/test_generator.py
@@ -21,30 +21,29 @@ class TestGenerator:
         with pytest.raises(ValidationError):
             Generator(vocs=test_vocs)
 
-    @pytest.mark.parametrize("generator_name", list(generators.keys()))
-    def test_serialization_loading(self, generator_name):
-        # Get generator defaults and class for this generator
-        gen_config = get_generator_defaults(generator_name)
-        gen_class = get_generator(generator_name)
+    @pytest.mark.parametrize("name", list(generators.keys()))
+    def test_serialization_loading(self, name):
+        gen_config = get_generator_defaults(name)
+        gen_class = get_generator(name)
 
-        # Handle special cases for different generator types
-        if generator_name in ["mobo", "cnsga", "mggpo"]:
+        if name in ["mobo", "cnsga", "mggpo"]:
             gen_config["reference_point"] = {"y1": 10.0}
-            test_vocs = TEST_VOCS_BASE
-        elif generator_name in ["multi_fidelity"]:
+            json.dumps(gen_config)
+
+            gen_class(vocs=TEST_VOCS_BASE, **gen_config)
+        elif name in ["multi_fidelity"]:
             test_vocs = deepcopy(TEST_VOCS_BASE)
             test_vocs.constraints = {}
-        elif generator_name in ["bayesian_exploration"]:
+            json.dumps(gen_config)
+
+            gen_class(vocs=test_vocs, **gen_config)
+        elif name in ["bayesian_exploration"]:
             test_vocs = deepcopy(TEST_VOCS_BASE)
             test_vocs.objectives = {}
             test_vocs.observables = ["f"]
+            json.dumps(gen_config)
+
+            gen_class(vocs=test_vocs, **gen_config)
         else:
-            test_vocs = TEST_VOCS_BASE
-
-        # Test JSON serialization
-        serialized = json.dumps(gen_config)
-        assert serialized is not None
-
-        # Test instantiation with the configuration
-        generator = gen_class(vocs=test_vocs, **gen_config)
-        assert generator is not None
+            json.dumps(gen_config)
+            gen_class(vocs=TEST_VOCS_BASE, **gen_config)
