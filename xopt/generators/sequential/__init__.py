9ff70fc2aebfbe56481d9bd1ed3029815267c262
Ryan Roussel <rroussel@slac.stanford.edu>
2025-03-21 14:40:05 -0500

    Update __init__.py

--- xopt/generators/sequential/__init__.py
+++ xopt/generators/sequential/__init__.py
@@ -1,5 +1,6 @@
+from xopt.generators.sequential.sequential_generator import SequentialGenerator
 from xopt.generators.sequential.rcds import RCDSGenerator
 from xopt.generators.sequential.extremumseeking import ExtremumSeekingGenerator
 from xopt.generators.sequential.neldermead import NelderMeadGenerator
 
-__all__ = ["RCDSGenerator", "ExtremumSeekingGenerator", "NelderMeadGenerator"]
+__all__ = ["SequentialGenerator", "RCDSGenerator", "ExtremumSeekingGenerator", "NelderMeadGenerator"]
