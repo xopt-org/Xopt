d7c30c3728500f327de4d0a0049fd99e611ceee6
Ryan Roussel <rroussel@slac.stanford.edu>
2025-03-28 16:54:38 -0500

    improve documentation for turbo controllers

--- xopt/generators/bayesian/turbo.py
+++ xopt/generators/bayesian/turbo.py
@@ -71,15 +71,17 @@ class TurboController(XoptBaseModel, ABC):
         Reset the controller to the initial state.
     """
 
-    vocs: VOCS = Field(exclude=True)
-    dim: PositiveInt
+    vocs: VOCS = Field(exclude=True, description="VOCS object")
+    dim: PositiveInt = Field(
+        None, description="number of dimensions in the optimization problem"
+    )
     batch_size: PositiveInt = Field(1, description="number of trust regions to use")
     length: float = Field(
         0.25,
         description="base length of trust region",
         ge=0.0,
     )
-    length_min: PositiveFloat = 0.5**7
+    length_min: PositiveFloat = Field(0.5**7, description="minimum base length of trust region")
     length_max: PositiveFloat = Field(
         2.0,
         description="maximum base length of trust region",
@@ -93,7 +95,7 @@ class TurboController(XoptBaseModel, ABC):
         None,
         description="number of successes to trigger a trust region contraction",
     )
-    center_x: Optional[Dict[str, float]] = Field(None)
+    center_x: Optional[Dict[str, float]] = Field(None, description="center point of trust region")
     scale_factor: float = Field(
         2.0, description="multiplier to increase or decrease trust region", ge=1.0
     )
@@ -268,8 +270,8 @@ class OptimizeTurboController(TurboController):
         Update the state of the controller.
     """
 
-    name: str = Field("OptimizeTurboController", frozen=True)
-    best_value: Optional[float] = None
+    name: str = Field("OptimizeTurboController", frozen=True, description="name of the Turbo controller")
+    best_value: Optional[float] = Field(None, description="best objective value found so far")
 
     @field_validator("vocs", mode="after")
     def vocs_validation(cls, info):
@@ -387,11 +389,20 @@ class SafetyTurboController(TurboController):
         Validate the VOCS for the controller.
     update_state(self, generator, previous_batch_size: int = 1)
         Update the state of the controller.
+
+
+    Notes:
+    ------
+    The trust region of the safety turbo controller is expanded or contracted based on the feasibility of the observed points. 
+    In cases where multiple samples are taken at once, the feasibility fraction is calculated based on the last 
+    `previous_batch_size` samples. If the feasibility fraction is above `min_feasible_fraction`, 
+    the observation is considered a success, otherwise it is a failure.
+    
     """
 
-    name: str = Field("SafetyTurboController", frozen=True)
+    name: str = Field("SafetyTurboController", frozen=True, description="name of the Turbo controller")
     scale_factor: PositiveFloat = 1.25
-    min_feasible_fraction: PositiveFloat = 0.75
+    min_feasible_fraction: PositiveFloat = Field(0.75, description="minimum feasible fraction to trigger trust region expansion/contraction")
 
     @field_validator("vocs", mode="after")
     def vocs_validation(cls, info):
