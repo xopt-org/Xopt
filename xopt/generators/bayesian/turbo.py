86a7cc4bd542e5258c0bcd6c57b4ee05eced557a
Ryan Roussel <rroussel@slac.stanford.edu>
2025-04-09 16:03:15 +0200

    simplify success/failure logic

--- xopt/generators/bayesian/turbo.py
+++ xopt/generators/bayesian/turbo.py
@@ -370,7 +370,7 @@ class OptimizeTurboController(TurboController):
             Y_last = recent_f_data_minform[self.vocs.objective_names[0]].min()
             best_value = self.best_value if self.minimize else -self.best_value
 
-            if Y_last <= best_value + 1e-3 * math.fabs(best_value):
+            if Y_last <= best_value:
                 self.success_counter += 1
                 self.failure_counter = 0
             else:
