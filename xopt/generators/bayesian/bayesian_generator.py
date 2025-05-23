36349e6f0a47f23ba28e2000331ff0ec4fa03061
nikitakuklev <spontaneouscoder@gmail.com>
2025-05-22 20:42:52 -0500

    Fix optimum search

--- xopt/generators/bayesian/bayesian_generator.py
+++ xopt/generators/bayesian/bayesian_generator.py
@@ -573,17 +573,30 @@ class BayesianGenerator(Generator, ABC):
     def get_optimum(self):
         """select the best point(s) given by the
         model using the Posterior mean"""
-        c_posterior_mean = ConstrainedMCAcquisitionFunction(
+        acq = ConstrainedMCAcquisitionFunction(
             self.model,
             qUpperConfidenceBound(
                 model=self.model, beta=0.0, objective=self._get_objective()
             ),
             self._get_constraint_callables(),
         )
+        bounds = self._get_bounds()
 
-        result = self.numerical_optimizer.optimize(
-            c_posterior_mean, self._get_bounds(), 1
-        )
+        if self.fixed_features is not None:
+            acq = self._apply_fixed_features(acq)
+
+            indices = []
+            for idx, name in enumerate(self.vocs.variable_names):
+                if name not in self.fixed_features:
+                    indices += [idx]
+
+            bounds = bounds[:, indices]
+
+        bounds = bounds.to(**self.tkwargs)
+        acq = acq.to(**self.tkwargs)
+
+        # use default initial conditions for a global search
+        result = self.numerical_optimizer.optimize(acq, bounds, 1)
 
         return self._process_candidates(result)
 
@@ -746,9 +759,9 @@ class BayesianGenerator(Generator, ABC):
                     variable_names.remove(name)
         return variable_names
 
-    def _get_bounds(self):
+    def _get_bounds(self) -> torch.Tensor:
         """
-        Convert bounds from vocs to torch tensors
+        Convert bounds from vocs to torch tensors of shape 2 x d.
 
         Tensor stays on CPU
         """
@@ -792,13 +805,13 @@ class BayesianGenerator(Generator, ABC):
         # associated with that key
         if self.fixed_features is not None:
             # grab variable name indices that are NOT in fixed features
-            indicies = []
+            indices = []
             for idx, name in enumerate(self.vocs.variable_names):
                 if name not in self.fixed_features:
-                    indicies += [idx]
+                    indices += [idx]
 
             # grab indexed bounds
-            bounds = bounds.T[indicies].T
+            bounds = bounds[:, indices]
 
         bounds = bounds.to(**self.tkwargs)
         return bounds
