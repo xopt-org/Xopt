34fd94a51749e523117a37390cacbbd076ec3b03
Sara Miskovich <saramiskovich@gmail.com>
2025-11-04 14:14:43 -0800

    make docs workflow fail if any notebooks fail

--- scripts/execute_notebooks.bash
+++ scripts/execute_notebooks.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 NOTEBOOKS=$(find . -type f -name "*.ipynb" -not -path '*/.*')
-
 SKIP="Xparallel"
+FAILED=0
 
 echo $NOTEBOOKS
 
@@ -14,5 +14,11 @@ do
     fi
 
     echo "Executing $file"
-    jupyter nbconvert --to notebook --execute $file --inplace
+    jupyter nbconvert --to notebook --execute $file --inplace --ExecutePreprocessor.raise_on_error=True
+    if [[ $? -ne 0 ]]; then
+        echo "FAILED: $file"
+        FAILED=1
+    fi
 done
+
+exit $FAILED
