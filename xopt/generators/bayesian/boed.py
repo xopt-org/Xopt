8729f8b45aad2079940312522ae63d4559faa0aa
Ryan Roussel <rroussel@slac.stanford.edu>
2025-12-04 13:14:59 -0600

    corrections for bugfixes

--- xopt/generators/bayesian/boed.py
+++ xopt/generators/bayesian/boed.py
@@ -1,6 +1,7 @@
 import pyro
 import pyro.distributions as dist
 import torch
+import logging
 from pydantic import PositiveFloat
 from pyro.contrib.oed.eig import marginal_eig
 from pyro.infer import SVI, Trace_ELBO
@@ -8,6 +9,8 @@ from pyro.optim import Adam
 from pyro.infer import Predictive
 
 from xopt.generators.bayesian.bayesian_generator import BayesianGenerator
+
+logger = logging.getLogger(__name__)
 
 
 class BOEDGenerator(BayesianGenerator):
@@ -62,10 +65,8 @@ class BOEDGenerator(BayesianGenerator):
             The number of candidate experiments to generate.
         """
         # start with training a model with historical data
-        xs = torch.tensor(self.data[self.vocs.variable_names].values).float().flatten()
-        ys = (
-            torch.tensor(self.data[self.vocs.observable_names].values).float().flatten()
-        )
+        xs = torch.tensor(data[self.vocs.variable_names].values).float().flatten()
+        ys = torch.tensor(data[self.vocs.observable_names].values).float().flatten()
 
         if len(self.history) == 0:
             self.history.append(self.get_probabilistic_model_parameters())
@@ -76,9 +77,8 @@ class BOEDGenerator(BayesianGenerator):
         svi = SVI(
             conditioned_model,
             guide,
-            Adam({"lr": 0.005}),
+            Adam({"lr": 0.01}),
             loss=Trace_ELBO(),
-            num_samples=100,
         )
         num_iters = 2000
         for i in range(num_iters):
@@ -91,13 +91,11 @@ class BOEDGenerator(BayesianGenerator):
         self.history.append(learned_params)
 
         for name, value in learned_params.items():
-            print(f"{name}: {value}")
+            logger.debug(f"{name}: {value}")
 
+        return_sites = ["y", "_RETURN"] + list(self.model_priors.keys())
         predictive = Predictive(
-            conditioned_model, 
-            guide=guide, 
-            num_samples=800,
-            return_sites=("y", "_RETURN")
+            current_model, guide=guide, num_samples=800, return_sites=return_sites
         )
 
         return predictive, learned_params
@@ -112,74 +110,96 @@ class BOEDGenerator(BayesianGenerator):
         current_model = self.get_model(learned_params)
 
         # now use the trained model to select new experiments
-        pyro.clear_param_store()
-
-        num_steps, start_lr, end_lr = 1000, 0.1, 0.001
         candidate_designs = torch.linspace(0, 6, 100).unsqueeze(-1)
-        optimizer = pyro.optim.ExponentialLR(
-            {
-                "optimizer": torch.optim.Adam,
-                "optim_args": {"lr": start_lr},
-                "gamma": (end_lr / start_lr) ** (1 / num_steps),
-            }
-        )
 
-        eig = marginal_eig(
-            current_model,
-            candidate_designs,
-            "y",
-            ["x0", "w", "b"],
-            num_samples=100,  # number of mc samples for estimating the EIG
-            num_steps=num_steps,
-            guide=self.get_marginal_guide(),
-            optim=optimizer,
-            final_num_samples=10000,  # number of iterations for minimizing the KL divergence of the marginal distribution
-        )
+        acqf = self._get_acquisition(current_model)
+        eig = acqf(candidate_designs)
+
+        # handle nan values in log eig
+        eig = torch.nan_to_num(eig, nan=float("-inf"))
+
         best_x = candidate_designs[torch.argmax(eig).detach()]
 
         return [
             {self.vocs.variable_names[0]: best_x.item()} for _ in range(n_candidates)
         ]
 
+    def _get_acquisition(self, model):
+        """
+        return a function that computes the log expected information gain
+
+        Parameters:
+        -----------
+        model : callable
+            The probabilistic model for which to compute the acquisition function.
+        """
+
+        def acquisition(x):
+            pyro.clear_param_store()
+            num_steps, start_lr, end_lr = 1000, 0.1, 0.001
+            optimizer = pyro.optim.ExponentialLR(
+                {
+                    "optimizer": torch.optim.Adam,
+                    "optim_args": {"lr": start_lr},
+                    "gamma": (end_lr / start_lr) ** (1 / num_steps),
+                }
+            )
+
+            eig = marginal_eig(
+                model,
+                x,
+                "y",
+                list(self.model_priors.keys()),
+                num_samples=100,  # number of mc samples for estimating the EIG
+                num_steps=num_steps,
+                guide=self.get_marginal_guide(),
+                optim=optimizer,
+                final_num_samples=10000,  # number of iterations for minimizing the KL divergence of the marginal distribution
+            )
+            return eig.log()
+
+        return acquisition
+
     def get_model(self, parameters: dict[str, torch.Tensor]):
         # define probabilistic model
         def model(x):
             # dim of -1 in l represents the number of measurements
             # other dims are batch dims
+
+            # sample parameters
+            sampled_params = {}
+            for name, distribution in self.model_priors.items():
+                if isinstance(distribution, dist.Normal):
+                    sampled_params[name] = pyro.sample(
+                        name,
+                        dist.Normal(
+                            parameters[f"{name}_loc"],
+                            parameters[f"{name}_scale"],
+                        ),
+                    )
+                elif isinstance(distribution, dist.Gamma):
+                    sampled_params[name] = pyro.sample(
+                        name,
+                        dist.Gamma(
+                            parameters[f"{name}_concentration"],
+                            parameters[f"{name}_rate"],
+                        ),
+                    )
+
+            # parameters are the same for each measurement
+            for key in sampled_params:
+                sampled_params[key] = sampled_params[key].unsqueeze(-1)
+
+            # get model output
+            output = self.model_function(x, **sampled_params)
+
             with pyro.plate_stack("data", x.shape[:-1]):
-                # sample parameters
-                sampled_params = {}
-                for name, distribution in self.model_priors.items():
-                    if isinstance(distribution, dist.Normal):
-                        sampled_params[name] = pyro.sample(
-                            name,
-                            dist.Normal(
-                                parameters[f"{name}_loc"],
-                                parameters[f"{name}_scale"],
-                            ),
-                        )
-                    elif isinstance(distribution, dist.Gamma):
-                        sampled_params[name] = pyro.sample(
-                            name,
-                            dist.Gamma(
-                                parameters[f"{name}_concentration"],
-                                parameters[f"{name}_rate"],
-                            ),
-                        )
-
-                # parameters are the same for each measurement
-                for key in sampled_params:
-                    sampled_params[key] = sampled_params[key].unsqueeze(-1)
-
-                # get model output
-                output = self.model_function(x, **sampled_params)
-
                 # observe data
                 y = pyro.sample(
                     "y",
                     dist.Normal(output, self.measurement_noise).to_event(1),
                 )
-                return y
+                return output
 
         return model
 
@@ -229,6 +249,3 @@ class BOEDGenerator(BayesianGenerator):
             pyro.sample("y", dist.Normal(q_y, self.measurement_noise).to_event(1))
 
         return marginal_guide
-
-    def _get_acquisition(self, model):
-        pass
