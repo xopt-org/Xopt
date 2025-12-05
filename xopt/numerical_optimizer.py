bugfix for numerical optimizer minimizing functions

xopt/numerical_optimizer.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any, Optional
 
+from matplotlib import pyplot as plt
 import torch
 from botorch.acquisition import AcquisitionFunction
 from botorch.optim import optimize_acqf
@@ -206,12 +207,19 @@ class GridOptimizer(NumericalOptimizer):
         ]
 
         xx = torch.meshgrid(*linspace_list, indexing="ij")
-        mesh_pts = torch.stack(xx).flatten(start_dim=1).T.double()
+
+        if dim == 1:
+            mesh_pts = linspace_list[0].double()
+        else:
+            mesh_pts = torch.stack(xx).flatten(start_dim=1).T.double()
 
         # evaluate the function on grid points
         f_values = function(mesh_pts.unsqueeze(1))
 
         # get the best n_candidates
-        _, indicies = torch.sort(f_values)
-        x_min = mesh_pts[indicies.squeeze().flipud()]
-        return x_min[:n_candidates]
+        _, indicies = torch.sort(f_values.squeeze(), descending=True)
+        x_max = mesh_pts[indicies][:n_candidates]
+        if dim == 1:
+            x_max = x_max.unsqueeze(-1)
+
+        return x_max

xopt/tests/test_numerical_optimizer.py
@@ -18,8 +18,7 @@ def f(X):
     # q_reduction converts it into `sample_shape x batch_shape`
     # sample_reduction converts it into `batch_shape`-dim Tensor of acquisition values
     # so, final fake tensor needs to have ndim=1
-    result = torch.amax(X, dim=(1, 2))
-    return result
+    return -torch.sum((X - 0.5) ** 2, dim=-1)
 
 
 class TestNumericalOptimizers:
@@ -54,6 +53,19 @@ class TestNumericalOptimizers:
             for ncandidate in [1, 3]:
                 candidates = optimizer.optimize(f, bounds, ncandidate)
                 assert candidates.shape == torch.Size([ncandidate, ndim])
+
+    def test_grid_optimizer_numerical_result(self):
+        # test quadratic function
+        def g(X):
+            return -torch.sum((X - 0.5) ** 2, dim=-1)
+
+        for ndim in [1, 2, 3]:
+            optimizer = GridOptimizer(n_grid_points=21)
+            bounds = torch.stack((torch.zeros(ndim), torch.ones(ndim)))
+            candidates = optimizer.optimize(g, bounds, n_candidates=1)
+            assert torch.allclose(
+                candidates, torch.tensor([0.5] * ndim).double(), atol=1e-2
+            )
 
     def test_in_xopt(self):
         vocs = deepcopy(tnk_vocs)