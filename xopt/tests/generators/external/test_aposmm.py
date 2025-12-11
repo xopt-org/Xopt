5278eb7231209f2037954d5486f8e001cab0362c
jlnav <jnavarro@anl.gov>
2025-12-11 09:17:32 -0600

    finish up test-case

--- xopt/tests/generators/external/test_aposmm.py
+++ xopt/tests/generators/external/test_aposmm.py
@@ -1,10 +1,9 @@
 from libensemble.gen_classes.aposmm import APOSMM
-import numpy as np
 from xopt import Xopt
 from xopt import Evaluator
 from gest_api.vocs import VOCS
-from copy import deepcopy
 import pytest
+
 
 def six_hump_camel_func(x):
     """Six-Hump Camel function definition"""
@@ -16,6 +15,7 @@ def six_hump_camel_func(x):
 
     return term1 + term2 + term3
 
+
 def evaluator_function(inputs):
     return {"f": six_hump_camel_func([inputs["x0"], inputs["x1"]])}
 
@@ -24,30 +24,81 @@ def evaluator_function(inputs):
 def evaluator():
     return Evaluator(function=evaluator_function)
 
+
 @pytest.fixture
 def vocs():
     return VOCS(
         variables={
-            "x0": [-2.0,2.0],
-            "x1": [-1.0,1.0],
-            "x0_on_cube": [0.0,1.0],
-            "x1_on_cube": [0.0,1.0],
-            },
+            "x0": [-2.0, 2.0],
+            "x1": [-1.0, 1.0],
+            "x0_on_cube": [0.0, 1.0],
+            "x1_on_cube": [0.0, 1.0],
+        },
         objectives={"f": "MINIMIZE"},
-        )
+    )
+
 
 @pytest.fixture
 def mapping():
     return {
-            "x": ["x0", "x1"],
-            "x_on_cube": ["x0_on_cube", "x1_on_cube"],
-        }
+        "x": ["x0", "x1"],
+        "x_on_cube": ["x0_on_cube", "x1_on_cube"],
+    }
+
 
 class TestXoptPlusAPOSMM:
+    def test_init(self, vocs, evaluator, mapping):
+        gen = APOSMM(
+            vocs=vocs,
+            max_active_runs=1,
+            initial_sample_size=40,
+            variables_mapping=mapping,
+        )
+        Xopt(generator=gen, evaluator=evaluator, vocs=vocs)
 
-    def test_init_with_aposmm(self, vocs, evaluator, mapping):
-        
-        gen = APOSMM(vocs=vocs, max_active_runs=1, initial_sample_size=40, variables_mapping=mapping)
+    def test_run(self, vocs, evaluator, mapping):
+        gen = APOSMM(
+            vocs=vocs,
+            max_active_runs=1,
+            initial_sample_size=40,
+            variables_mapping=mapping,
+        )
+        x = Xopt(generator=gen, evaluator=evaluator, vocs=vocs, max_evaluations=45)
+        x.run()
+        x.generator.finalize()
+        assert x.data.shape[0] == 45
+        assert all(x.data["local_pt"][-5:]), (
+            "last 5 points should be local_pt, as communicated by APOSMM"
+        )
+
+    def test_random_evaluate(self, vocs, evaluator, mapping):
+        gen = APOSMM(
+            vocs=vocs,
+            max_active_runs=1,
+            initial_sample_size=40,
+            variables_mapping=mapping,
+        )
         x = Xopt(generator=gen, evaluator=evaluator, vocs=vocs)
-        import ipdb; ipdb.set_trace()
-        print(';liksdhfoijuasdhdfoiusah')
+        x.random_evaluate(40)
+        x.step()
+        x.generator.finalize()
+        assert x.data.shape[0] == 41
+        assert all(x.data["local_pt"][-1:]), (
+            "last point should be local_pt, as communicated by APOSMM"
+        )
+
+    def test_step(self, vocs, evaluator, mapping):
+        gen = APOSMM(
+            vocs=vocs,
+            max_active_runs=1,
+            initial_sample_size=40,
+            variables_mapping=mapping,
+        )
+        x = Xopt(generator=gen, evaluator=evaluator, vocs=vocs)
+        for i in range(45):
+            x.step()
+        x.generator.finalize()
+        assert x.data.shape[0] == 45
+        assert all(x.data["local_pt"][-5:]), (
+            "last 5 points should be local_pt, as communicated by APOSMM"
+        )
