8b80bb5d13ec5e89f571648a7b9838b42c3c4753
Michael Eliot <mieliot@protonmail.com>
2026-01-01 14:35:39 -0800

    fix: update to constrain output within bounds

--- xopt/generators/rl/model.py
+++ xopt/generators/rl/model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from xopt.generator import Generator
 from stable_baselines3.common.base_class import BaseAlgorithm
 from typing import List, Dict, Any, Union
+import numpy as np
 
 
 class ModelWrapper:
@@ -17,11 +18,6 @@ class ModelWrapper:
 
 
 class RLModelGenerator(Generator):
-    """
-    RL Model generator. Utilizes an RL model to generate the set of points for evaluation.
-    This is configured to handle multi-variable optimization.
-    """
-
     name = "rl_model_generator"
     supports_batch_generation: bool = True
     supports_multi_objective: bool = True
@@ -29,11 +25,11 @@ class RLModelGenerator(Generator):
     rl_model: ModelWrapper = None
 
     def set_model(self, model: BaseAlgorithm):
+        # TODO should verify the model adheres to the inputs/output expectations in vocs
         self.rl_model = ModelWrapper(model)
 
     @property
     def model_input_names(self) -> List[str]:
-        """variable names corresponding to trained model"""
         return self.vocs.variable_names
 
     def generate(self, n_candidates: int) -> List[Dict[str, Union[float, Any]]]:
@@ -43,25 +39,36 @@ class RLModelGenerator(Generator):
                 "method to set data, see also `Xopt.random_evaluate()`"
             )
 
-        model = self.rl_model.model
+        bounds = np.array(
+            [self.vocs.variables[name] for name in self.model_input_names]
+        )
+        mins = bounds[:, 0]
+        maxs = bounds[:, 1]
 
+        model = self.rl_model.model
         input_data = self.data[self.model_input_names]
 
-        current_states_array = input_data.values
+        current_state = input_data.iloc[-1].values
+
+        if current_state.ndim == 0:
+            current_state = np.array([current_state])
 
         candidates = []
-        for state in current_states_array:
-            action, _ = model.predict(state, deterministic=True)
+
+        for _ in range(n_candidates):
+            action, _states = model.predict(current_state, deterministic=False)
 
             if action.ndim > 1:
                 action = action.flatten()
 
-            new_candidate_state = state + action
+            new_candidate_state = current_state + action
+            new_candidate_state = np.clip(new_candidate_state, mins, maxs)
 
             candidates.append(new_candidate_state)
 
-        results = self.vocs.convert_numpy_to_inputs(candidates, include_constants=False)
+            current_state = new_candidate_state
 
+        results = self.vocs.convert_numpy_to_inputs(candidates, include_constants=False)
         return results
 
     def set_data(self, data: pd.DataFrame):
