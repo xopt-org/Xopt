b2d7dd044aaedb03ba57c6fb965b217a0ea4dfe0
nikitakuklev <spontaneouscoder@gmail.com>
2026-02-25 22:26:40 -0500

    fix lint E402 in bayesian __init__

--- xopt/generators/bayesian/__init__.py
+++ xopt/generators/bayesian/__init__.py
@@ -1,29 +1,29 @@
+import torch
+
+from xopt.generators.bayesian.bax_generator import BaxGenerator
 from xopt.generators.bayesian.bayesian_exploration import BayesianExplorationGenerator
+from xopt.generators.bayesian.bayesian_generator import BayesianGenerator
 from xopt.generators.bayesian.expected_improvement import (
     ExpectedImprovementGenerator,
     TDExpectedImprovementGenerator,
 )
+from xopt.generators.bayesian.mggpo import MGGPOGenerator
 from xopt.generators.bayesian.mobo import MOBOGenerator
 from xopt.generators.bayesian.multi_fidelity import MultiFidelityGenerator
+from xopt.generators.bayesian.time_dependent import TimeDependentBayesianGenerator
+from xopt.generators.bayesian.turbo import (
+    EntropyTurboController,
+    OptimizeTurboController,
+    SafetyTurboController,
+    TurboController,
+)
 from xopt.generators.bayesian.upper_confidence_bound import (
     TDUpperConfidenceBoundGenerator,
     UpperConfidenceBoundGenerator,
 )
-from xopt.generators.bayesian.mggpo import MGGPOGenerator
-from xopt.generators.bayesian.bax_generator import BaxGenerator
-from xopt.generators.bayesian.bayesian_generator import BayesianGenerator
-from xopt.generators.bayesian.time_dependent import TimeDependentBayesianGenerator
-import torch
 
 # set default precision
 torch.set_default_dtype(torch.double)
-
-from xopt.generators.bayesian.turbo import (
-    TurboController,
-    SafetyTurboController,
-    OptimizeTurboController,
-    EntropyTurboController,
-)
 
 __all__ = [
     "BayesianExplorationGenerator",
