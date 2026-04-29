Fixes bax acquisition function calculation to match changes to recent versions of botorch and adds tests to confirm accuracy.

xopt/tests/generators/bayesian/test_bax.py
@@ -124,18 +124,16 @@ class TestBaxGenerator:
             # need to call each sub-model on some data before conditioning
             [m(x_exe) for m in model.models]
 
-            x_exe_t = [
-                model.models[i].input_transform(x_exe) for i in range(len(model.models))
+            x_exe_list = [
+                x_exe for i in range(len(model.models))
             ]
-            y_exe_t = [
-                model.models[i].outcome_transform(
-                    torch.index_select(y_exe, dim=-1, index=torch.tensor([i]))
-                )[0]
+            y_exe_list = [
+                torch.index_select(y_exe, dim=-1, index=torch.tensor([i]))
                 for i in range(len(model.models))
             ]
             fantasy_models = [
                 m.condition_on_observations(x, y)
-                for m, x, y in zip(model.models, x_exe_t, y_exe_t)
+                for m, x, y in zip(model.models, x_exe_list, y_exe_list)
             ]
 
             # validate fantasy models
@@ -166,6 +164,56 @@ class TestBaxGenerator:
             y_max, idx_max = torch.max(posterior_samples, dim=-2)
             assert torch.allclose(y_exe.squeeze(-2), y_max)
             assert x_exe.shape[0] == alg.n_samples
+
+    def test_acquisition_func(self):
+        torch.manual_seed(1)
+
+        alg = GridOptimize()
+
+        # test w/o constraints
+        test_vocs = deepcopy(TEST_VOCS_BASE)
+        test_vocs.objectives = {}
+        test_vocs.observables = ["y1"]
+        test_vocs.constraints = {}
+        gen = BaxGenerator(
+            vocs=test_vocs,
+            algorithm=alg,
+            algorithm_results_file="test",
+        )
+        gen.numerical_optimizer.n_restarts = 1
+        gen.data = TEST_VOCS_DATA
+
+        candidate = gen.generate(1)
+        assert len(candidate) == 1
+
+        # test w/ constraints
+        test_vocs = deepcopy(TEST_VOCS_BASE)
+        test_vocs.objectives = {}
+        test_vocs.observables = ["y1"]
+        gen = BaxGenerator(
+            vocs=test_vocs,
+            algorithm=alg,
+        )
+        gen.numerical_optimizer.n_restarts = 1
+        gen.data = TEST_VOCS_DATA
+
+        model = gen.train_model()
+        acqf = gen.get_acquisition(model)
+
+        n_grid = 3
+        test_mesh = torch.meshgrid(torch.linspace(*test_vocs.variables["x1"].domain, n_grid),
+                                   torch.linspace(*test_vocs.variables["x2"].domain, n_grid),
+                                   indexing='ij',
+                                  )
+        test_x = torch.stack((test_mesh[0].flatten(),
+                              test_mesh[1].flatten()),
+                              dim=-1,
+                            )
+        acqf_vals = acqf(test_x.reshape(-1, 1, 2))
+        # numerical benchmarking of bax algorithm/acquisition function values in lieu of analytical form
+        test_acqf_vals = torch.tensor([-14.1820,  -6.5169,  -4.7833,  -6.7005,  -8.0354,  -4.7237,  -4.9605,
+                 -4.8018,  -9.2135])
+        assert torch.allclose(acqf_vals, test_acqf_vals, atol=1e-4)
 
     def test_generate(self):
         alg = GridOptimize()